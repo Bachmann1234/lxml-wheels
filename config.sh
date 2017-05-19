9796711be5ccd31785834562ad4f946b2618a5f0
Matt Bachmann <bachmann.matt@gmail.com>
2017-05-19 08:10:10 -0400

    initial commit

---
+++ config.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    :
+}
+
+function run_tests {
+    # Runs tests on installed distribution from an empty directory
+    python --version
+    python -c 'import sys; import lxml; sys.exit(lxml.test())'
+}
