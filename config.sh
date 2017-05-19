30b51c11d37f1f9805791acb7dfe91e034cb33fe
Matt Bachmann <bachmann.matt@gmail.com>
2017-05-19 08:10:10 -0400

    Setup build system to build wheels for a whole bunch of platforms

---
+++ config.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Define custom utilities
+# Test for OSX with [ -n "$IS_OSX" ]
+#function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+#}
+
+function bdist_with_static_deps {
+    local abs_wheelhouse=$1
+    python setup.py clean
+    if ! [ -n "$IS_OSX" ]; then
+        CFLAGS="-O3 -mtune=generic -pipe -fPIC";export CFLAGS
+    fi
+    make wheel_static
+    cp dist/*.whl $abs_wheelhouse
+}
+
+function build_wheel {
+    build_wheel_cmd "bdist_with_static_deps" $@
+}
+
+function run_tests {
+    # Runs tests on installed distribution from an empty directory
+    python --version
+    python -c 'import sys; import lxml.etree, lxml.objectify'
+}
