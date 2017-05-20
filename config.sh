c5b6d0e71ec9976e7f9e253c85164baa3f87383a
Matt Bachmann <bachmann.matt@gmail.com>
2017-05-20 08:33:42 -0400

    install libraries

--- config.sh
+++ config.sh
@@ -2,11 +2,12 @@
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-    :
+    if [ -n "$IS_OSX" ]; then return; fi
+    yum install -y libxml2 libxlst1 libxslt1-dev python-dev
 }
 
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
-    python -c 'import sys; import lxml; sys.exit(lxml.test())'
+    python -c 'import sys; import lxml.etree, lxml.objectify; sys.exit(lxml.test())'
 }
